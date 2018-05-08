3cffaf9a0060c9de3625199b47579a8b5af7db00
Islam Attrash <iattrash@vasonanetworks.com>
2018-05-08 18:15:46 +0300

    Better examples

--- examples/example01.ts
+++ examples/example01.ts
@@ -3,12 +3,26 @@ import { ValidationPipe } from "../src/ValidationPipe";
 import _isUndefined from "lodash-es/isUndefined";
 import _isNull from "lodash-es/isNull";
 
-export const HexaDecimalValidator: IValidator = value => /^[0-9A-Fa-f]+$/.test(value);
+const HexaDecimalValidator: IValidator = value => /^[0-9A-Fa-f]+$/.test(value);
 
-export const StringLimitedLengthValidator = (maxLength: number) => (value: string) => value.length < maxLength;
+const StringLimitedLengthValidator = (maxLength: number) => (value: string) => value.length < maxLength;
 
-export const HexaDecaLimitedLengthValidator =
-  ValidationPipe([HexaDecimalValidator, StringLimitedLengthValidator(10)]);
+const StringLimitedUpTo10 = StringLimitedLengthValidator(10);
 
-export const isNotNullNorUndefinedValidator =
+const HexaDecaLimitedLengthValidator =
+  ValidationPipe([HexaDecimalValidator, StringLimitedUpTo10]);
+
+// We'll use it on a valid hexadecimal value but not valid by the string length limitation
+const useTheComplexAbove = HexaDecaLimitedLengthValidator("fffff4654535435434353");
+
+// Console into the log the failedPipes array which points to the validationPipe functions
+console.log("FailedPipes", useTheComplexAbove.failedPipes); // 1
+console.log("valid final result", useTheComplexAbove.valid); // false
+
+// You can use equality to find which pipes failed to determine what to do next
+// useTheComplexAbove.failedPipes[0] === StringLimitedUpTo10 resolves as true
+// or maybe indexOf? Other complex computations?
+
+// Usage with External Libraries that implement the same IValidator interface -> (value: any) => boolean
+const isNotNullNorUndefinedValidator =
   ValidationPipe([x => !_isUndefined(x), x => !_isNull(x)]);
